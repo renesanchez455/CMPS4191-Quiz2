183ad389d2b436f65470cf82a8337838049f5407
renesanchez455 <renesanchez455@yahoo.com>
2022-10-07 18:07:49 -0600

    modified html and css files, added all elm code for star and compiled

--- star/src/Star.elm
+++ star/src/Star.elm
@@ -1,7 +1,64 @@
+-- CMPS4191 - Quiz #2
+-- Rene Sanchez - 2018118383
+
 module Star exposing (main)
 
-import Html exposing (Html, div, text)
+import Html exposing (..)
+import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
+import Browser
 
-main : Html msg
-main =
-    div [] [ text "Star" ]
+type Msg = 
+    Like | Unlike
+
+initialModel : { liked : Bool }
+initialModel = 
+    {
+        liked = False
+    }
+viewStar : { liked : Bool } -> Html Msg
+viewStar model = 
+    let
+        buttonType = 
+            if model.liked then "star" else "star_border"
+        msg =
+                if model.liked then Unlike else Like
+    in
+    
+    div [ class "star-button" ]
+        [ span
+            [ class "material-icons md-48", onClick msg ]
+            [ text buttonType ]
+        ]
+
+
+view : {liked : Bool } -> Html Msg
+view model =
+    div []
+    [
+        div [ class "header" ]
+        [ h1 [] [text "Star" ] ]
+        ,div [ class "content-flow" ]
+        [
+            viewStar model
+        ]
+    ]
+
+update : Msg -> {liked : Bool } -> {liked : Bool }
+update msg model = 
+    case msg of
+        Like ->
+            { model | liked = True }
+        Unlike ->
+            { model | liked = False }
+
+
+
+main : Program () {liked : Bool } Msg
+main = 
+    Browser.sandbox
+    {
+        init = initialModel
+        ,view = view
+        ,update = update
+    }
